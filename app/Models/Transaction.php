62013cc353e98c6f73668ef46e9ff217af5d9fa9
TuAlex <nguyendinhtubk20@gmail.com>
2024-05-03 13:12:46 +0000

    Modify transaction model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -6,7 +6,15 @@ class Transaction extends model
         parent::__construct();
     }
 
-    public function addTransaction() {
-        $query = $this->_db->query('INSERT INTO transaction (user_id, cash) VALUES (?,?)', []);
+    public function addTransaction($user_id, $cash) {
+        $query = $this->_db->query('INSERT INTO transaction (user_id, cash) VALUES (?,?)', [$user_id, $cash]);
+        return true;
+    }
+
+    public function transfer($transimitter, $reciever, $cash) {
+        $query1 = $this->_db->query('INSERT INTO transaction (user_id, cash) VALUES (?,?)', [$transimitter, $cash]);
+        $query2 = $this->_db->query('INSERT INTO transaction (user_id, cash) VALUES (?,?)', [$reciever, $cash]);
+
+        return true;
     }
 }
